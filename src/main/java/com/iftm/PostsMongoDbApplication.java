d667205f6b8edc61ad06f5891b824cb100d8877a
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-09-13 11:44:36 -0300

    Project created

---
+++ src/main/java/com/iftm/PostsMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.iftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/PostsMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
