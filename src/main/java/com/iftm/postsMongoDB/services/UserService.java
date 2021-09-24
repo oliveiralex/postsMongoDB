3c4513fff0c871ea05dab3bc6425d6acbf7f5005
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-09-24 19:33:11 -0300

    GET User by id

--- src/main/java/com/iftm/postsMongoDB/services/UserService.java
+++ src/main/java/com/iftm/postsMongoDB/services/UserService.java
@@ -1,6 +1,7 @@
 package com.iftm.postsMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.iftm.postsMongoDB.models.dto.UserDTO;
 import com.iftm.postsMongoDB.models.entities.User;
 import com.iftm.postsMongoDB.repositories.UserRepository;
+import com.iftm.postsMongoDB.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new UserDTO(entity);
 	}
 }
